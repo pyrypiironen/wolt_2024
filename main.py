c171066466bf1e6a2b1dd0165036e4dbac34b3b2
Pyry-Peetri Piironen <pyrypiironen@gmail.com>
2024-01-29 19:11:28 +0200

    Application v1.0 done, but not tested.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, conint
 from datetime import date, datetime, time
+from typing import Union
 from zoneinfo import ZoneInfo
 import pytz
 
@@ -9,36 +10,53 @@ import pytz
 
 app = FastAPI(title="Delivery Fee Calculator")
 
-class Payload(BaseModel):
+class Request_Payload(BaseModel):
 	cart_value: conint(ge=0)
 	delivery_distance: conint(ge=0)
 	number_of_items: conint(ge=0)
 	time: str
 
-class Response(BaseModel):
+class Response_Payload(BaseModel):
 	delivery_fee: int
 
 
-# Post endpoint
-@app.post("/delivery_fee/", response_model=Response)
-async def make_response(payload: Payload):
-	fee = delivery_fee_calculator(payload)
-	return Response(delivery_fee=fee)
+
+
+@app.post("/delivery_fee/", response_model=Response_Payload)
+async def make_Response_Payload(Request_Payload: Request_Payload):
+	fee = delivery_fee_calculator(Request_Payload)
+	if fee == None:
+		raise HTTPException(status_code = 400, detail = "Error: Time string isn't in ISO format.")
+	return Response_Payload(delivery_fee = fee)
 
 
 
-def delivery_fee_calculator(payload):
-
-	fee = 0
-	fee += get_small_order_surcharge(payload.cart_value)
-	fee += get_delivery_distance_fee(payload.delivery_distance)
-	fee += get_items_surcharge(payload.number_of_items)
-	fee *= get_friday_rush_multiplier(payload.time)
-	fee = fee_cutter(fee, payload.cart_value)
-
+def delivery_fee_calculator(Request_Payload):
+	rush_multiplier = get_friday_rush_multiplier(Request_Payload.time)
+	if rush_multiplier == None:
+		return None
+	fee = get_delivery_distance_fee(Request_Payload.delivery_distance)
+	fee += get_small_order_surcharge(Request_Payload.cart_value)
+	fee += get_items_surcharge(Request_Payload.number_of_items)
+	fee *= rush_multiplier
+	fee = fee_cutter(fee, Request_Payload.cart_value)
 
 
 	return int(fee)
+
+
+
+
+
+###
+def get_delivery_distance_fee(delivery_distance):
+	fee = 0
+	while delivery_distance > 0:
+		fee += 100
+		delivery_distance -= 500
+	return min(2, fee)
+
+
 
 
 ###
@@ -46,72 +64,36 @@ def get_small_order_surcharge(cart_value):
 	return max(1000 - cart_value, 0)
 
 
-###
-def get_delivery_distance_fee(distance):
-	fee = 200
-	distance -= 1000
-
-
-	return fee
 
 
 ###
-def get_items_surcharge(number_of_items):
-
-	#remember bulk fee
-	return 101
-
+def get_items_surcharge(items):
+	fee = 0
+	bulk_fee = 0
+	if items > 4:
+		fee = (items - 4) * 50
+		if items > 12:
+			bulk_fee = 120
+	return fee + bulk_fee
 
 ###
 def get_friday_rush_multiplier(time):
-	time_object = datetime.fromisoformat(time)
-	start_rush = time_object.replace(hour = 15, minute = 0, second = 0)
-	end_rush = time_object.replace(hour = 19, minute = 0, second = 0)
-	utc_time_object = time_object.astimezone(ZoneInfo("UTC"))					## Check usage and del /// Only utc
-	weekday = time_object.strftime("%A")										## Weekday
-	
-	
-	## ## ## Testing
-	print("--------------------------------------------------------------")
-	print("time_object")
-	print(time_object)
-	print()
-	print("utc_time_object --- we are not using this now")
-	print(utc_time_object)
-	print()
-	print(start_rush)
-	print()
-	print(weekday)
-	print()
-	if weekday == "Friday":
-		print("It really is a Friday")
-	else:
-		print("Today is " + weekday)
-	
-	print()
-	weekdayAsNum = time_object.weekday()
-	print("Weekday as number is " + str(weekdayAsNum))
-	print("--------------------------------------------------------------")
-
-
-	## ## ##
-	if weekday == "Friday":
-		if start_rush <= time_object <= end_rush:
-			print("It is Friday rush!")
-			print("get_friday_rush returned 1.2")
-			print("--------------------------------------------------------------")
-			return 1.2
+	try:
+		dt_object = datetime.fromisoformat(time)
+		start_rush = dt_object.replace(hour = 15, minute = 0, second = 0)
+		end_rush = dt_object.replace(hour = 19, minute = 0, second = 0)
+		weekday = dt_object.strftime("%A")
+		if weekday == "Friday":
+			if start_rush <= dt_object <= end_rush:
+				return 1.2
+			else:
+				return 1
 		else:
-			print("It is Friday, but not between 3 PM and 7 PM!")
-			print("get_friday_rush returned 1")
-			print("--------------------------------------------------------------")
 			return 1
-			
-	else:
-		print("It is not Friday!")
-		print("get_friday_rush returned 1")
-		print("--------------------------------------------------------------")
-		return 1
+	except ValueError:
+		return None
+	
+
 
 ###
 def	fee_cutter(fee, cart_value):
